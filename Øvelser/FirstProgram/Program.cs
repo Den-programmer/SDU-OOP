fc0bd1217d4fd264fdb95cb2600f51ac9e184270
DEnis Grigorov <denisgrigorov094@gmail.com>
2026-09-10 11:33:01 +0200

    First program, circle's areal and length

---
+++ Øvelser/FirstProgram/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+// At finde circle's area
+// string radiusString = Console.ReadLine();
+// int radius = int.Parse(radiusString);
+// double circleAreal = radius*radius*Math.PI;
+// Console.WriteLine(circleAreal);
+
+
+// At finde circumference med radius n og dets sum
+
+// string radiusString1 = Console.ReadLine();
+// int radius1 = int.Parse(radiusString1);
+// double circleLength1 = 2*radius1*Math.PI;
+// Console.WriteLine("The length of the first circle is: " + circleLength1);
+
+// string radiusString2 = Console.ReadLine();
+// int radius2 = int.Parse(radiusString2);
+// double circleLength2 = 2*radius2*Math.PI;
+// Console.WriteLine("The length of the second circle is: " + circleLength2);
+
+// string radiusString3 = Console.ReadLine();
+// int radius3 = int.Parse(radiusString3);
+// double circleLength3 = 2*radius3*Math.PI;
+// Console.WriteLine("The length of the third circle is: " + circleLength3);
+
+// double sum = circleLength1 + circleLength2 + circleLength3;
+
+// Console.WriteLine("The sum of the three lengthes is: " + sum);
+
+
+
+
+
+
+
+
+
+
+
+
+// namespace MyAPP {
+
+//     class ADD_TEXT {
+//         const string text = "Hello, World! if you add these numbers, you will get the sum: ";
+//         public double addNumbers(double firstN, double secondN) {
+//             double sum = firstN + secondN;
+//             return text + sum.ToString();
+//         }   
+//     }
+// }
+
+// double firstParam = double.Parse(Console.ReadLine("Enter first number:"));
+// double secondParam = double.Parse(Console.ReadLine("Enter second number:"));
+
+// var obj1 = new ADD_TEXT();
+
+// Console.WriteLine(obj1.addNumbers(firstParam, secondParam));
+
+// int counter = 5;
+
+// Console.WriteLine(counter);
+// Console.WriteLine(++counter); // Pre increment: First counter += 1, bagefter output 
+// Console.WriteLine(counter);
+// Console.WriteLine(counter++); // Post increment: First output, bagefter counter += 1
+// Console.WriteLine(counter);
